520c9fb4b69f04ff2a79db0315a8d73b979e8d48
Leandro Mendes Tavares <ND.LeandroMendes@opovonet.com.br>
2018-03-05 15:26:15 -0300

    Add Specific Page Exemple

---
+++ page-exemplo.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+*
+* Template Specific Page
+*
+**/
+
+?>
+
+<h1>Página com HTML específico</h1>
+<hr>
+<h2><?php single_post_title() ?></h2>
+
+
+
+<?php
+	if (have_posts()): the_post();
+
+		the_content();
+
+	endif;	
+?>
+
+
+
+
+
+
+<!-- FOOTER -->
+<?php get_footer() ?>
+<!-- /FOOTER -->
