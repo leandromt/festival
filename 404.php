6e8139aabbae1de41827ace88ea1625ae0560fa5
Leandro Mendes Tavares <ND.LeandroMendes@opovonet.com.br>
2018-03-06 14:07:50 -0300

    Add Page 404

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+*
+* Template Error 404
+*
+**/
+?>
+
+<!-- HEADER -->
+<?php get_header() #get_header('personalizado') ?>
+<!-- /HEADER -->
+
+
+
+<h1>Ops! Erro</h1>
+<h2>404</h2>
+
+
+
+<!-- FOOTER -->
+<?php get_footer() ?>
+<!-- /FOOTER -->
